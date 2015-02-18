d1bba336d847026f38ddf09c1edfddf957a93cf8
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-02-18 16:52:11 +0300

    Implement error page

---
+++ page/error.php
@@ -0,0 +1,10 @@
+	<!-- Breadcrumbs -->
+	<?php include "../templates/breadcrumbs/error.html"; ?>
+	
+	<!-- Content -->
+	<div class="content with-breadcrumbs">
+		
+		<!-- Section -->
+		<?php include "../templates/content/error.html"; ?>
+
+	</div>
