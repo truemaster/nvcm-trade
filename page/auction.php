5abb74e8c587165e086c07df19132bea8268ef4c
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-03-23 19:15:03 +0300

    Fix side-panel 'Tab issue'

--- page/auction.php
+++ page/auction.php
@@ -13,9 +13,9 @@
 	
 	<!-- Side Panel -->
 	<div class="side-panel-list">
-		<div class="side-panel-list-item" onclick="$(this).parent().children().removeClass('active'); $(this).addClass('active'); $('.side-panel').removeClass('active'); $('#auction-categories').addClass('active');" title="Рубрикатор"><div class="icon icon-sitemap"></div></div>
-		<div class="side-panel-list-item" onclick="$(this).parent().children().removeClass('active'); $(this).addClass('active'); $('.side-panel').removeClass('active'); $('#auction-filter').addClass('active');" title="Фильтр данных"><div class="icon icon-filter"></div></div>
-		<div class="side-panel-list-item" onclick="$(this).parent().children().removeClass('active'); $(this).addClass('active'); $('.side-panel').removeClass('active'); $('#auction-settings').addClass('active');" title="Настройки списка"><div class="icon icon-cog"></div></div>
+		<div class="side-panel-list-item" onclick="openSidePanel('auction-categories'); $(this).addClass('active');" title="Рубрикатор"><div class="icon icon-sitemap"></div></div>
+		<div class="side-panel-list-item" onclick="openSidePanel('auction-filter'); $(this).addClass('active');" title="Фильтр данных"><div class="icon icon-filter"></div></div>
+		<div class="side-panel-list-item" onclick="openSidePanel('auction-settings'); $(this).addClass('active');" title="Настройки списка"><div class="icon icon-cog"></div></div>
 	</div>
 	<?php include "../templates/side-panel/auction-filter.html"; ?>
 	<?php include "../templates/side-panel/auction-settings.html"; ?>
