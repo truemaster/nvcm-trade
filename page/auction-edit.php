1b6de20a724767e670756e415bb90f84e9775367
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-03-23 19:15:14 +0300

    .

---
+++ page/auction-edit.php
@@ -0,0 +1,10 @@
+	<!-- Breadcrumbs -->
+	<?php include "../templates/breadcrumbs/auction.html"; ?>
+	
+	<!-- Content -->
+	<div class="content with-breadcrumbs">
+		
+		<!-- Section -->
+		<?php include "../templates/content/auction-edit.html"; ?>
+
+	</div>
