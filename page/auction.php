263080d3b7438701fa4bbd32f8fdb5e4b2e1e7ec
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-03-26 17:41:19 +0300

    Add side panel with list

--- page/auction.php
+++ page/auction.php
@@ -8,15 +8,9 @@
 		<?php include "../templates/content/auction.html"; ?>
 
 	</div>
-	
 
-	
 	<!-- Side Panel -->
 	<div class="side-panel-list">
-		<div class="side-panel-list-item" onclick="openSidePanel('auction-categories'); $(this).addClass('active');" title="Рубрикатор"><div class="icon icon-sitemap"></div></div>
-		<div class="side-panel-list-item" onclick="openSidePanel('auction-filter'); $(this).addClass('active');" title="Фильтр данных"><div class="icon icon-filter"></div></div>
-		<div class="side-panel-list-item" onclick="openSidePanel('auction-settings'); $(this).addClass('active');" title="Настройки списка"><div class="icon icon-cog"></div></div>
+		<div class="side-panel-list-item" onclick="openSidePanel('list'); $(this).addClass('active');" title="Список"><div class="icon icon-list"></div></div>
 	</div>
-	<?php include "../templates/side-panel/auction-filter.html"; ?>
-	<?php include "../templates/side-panel/auction-settings.html"; ?>
-	<?php include "../templates/side-panel/auction-categories.html"; ?>
+	<?php include "../templates/side-panel/list.html"; ?>
