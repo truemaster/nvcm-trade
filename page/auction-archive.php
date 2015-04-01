736fccec9674c4f8e3e2ff944e6e3eb731e33473
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-04-01 17:11:52 +0300

    .

---
+++ page/auction-archive.php
@@ -0,0 +1,20 @@
+	<!-- Breadcrumbs -->
+	<?php include "../templates/breadcrumbs/auction-catalog-archive.html"; ?>
+	
+	<!-- Content -->
+	<div class="content with-side-panel">
+		
+		<!-- Section -->
+		<?php include "../templates/content/auction-catalog-table.html"; ?>
+
+	</div>
+	
+	<!-- Side Panel -->
+	<div class="side-panel-list">
+		<div class="side-panel-list-item" onclick="toggleSidePanel(this, 'auction-categories');" title="Рубрикатор"><div class="icon icon-sitemap"></div></div>
+		<div class="side-panel-list-item" onclick="toggleSidePanel(this, 'auction-filter');" title="Фильтр данных"><div class="icon icon-filter"></div></div>
+		<div class="side-panel-list-item" onclick="toggleSidePanel(this, 'auction-settings');" title="Настройки списка"><div class="icon icon-cog"></div></div>
+	</div>
+	<?php include "../templates/side-panel/auction-filter.html"; ?>
+	<?php include "../templates/side-panel/auction-settings.html"; ?>
+	<?php include "../templates/side-panel/auction-categories.html"; ?>
