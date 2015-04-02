05d1adef0a8a42625c05b7596b792172349cedca
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-04-02 13:58:35 +0300

    Update to FontAwesome 4.3

--- page/help-guide.php
+++ page/help-guide.php
@@ -11,6 +11,6 @@
 	
 			<!-- Side Panel -->
 	<div class="side-panel-list">
-		<div class="side-panel-list-item" onclick="openSidePanel(this, 'help-guide-table-of-content')" title="Оглавление"><div class="icon icon-sitemap"></div></div>
+		<div class="side-panel-list-item" onclick="openSidePanel(this, 'help-guide-table-of-content')" title="Оглавление"><div class="icon fa fa-sitemap"></div></div>
 	</div>
 	<?php include "../templates/side-panel/table-of-content.html"; ?>
