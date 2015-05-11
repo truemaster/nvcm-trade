280ef345e5f1329b307886f20d3019247d551c0c
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-05-11 17:59:34 +0300

    Add key-value view to side panel

--- page/auction-edit.php
+++ page/auction-edit.php
@@ -2,9 +2,15 @@
 	<?php include "../templates/breadcrumbs/auction.html"; ?>
 	
 	<!-- Content -->
-	<div class="content with-breadcrumbs">
+	<div class="content with-breadcrumbs with-side-panel">
 		
 		<!-- Section -->
 		<?php include "../templates/content/auction-edit.html"; ?>
 
 	</div>
+	
+	<!-- Side Panel -->
+	<div class="side-panel-list">
+		<div class="side-panel-list-item" onclick="toggleSidePanel(this, 'key-value');" title="Параметры публикации"><div class="icon fa fa-cogs"></div></div>
+	</div>
+	<?php include "../templates/side-panel/key-value.html"; ?>
