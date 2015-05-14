69316a96255668ae6a55946d1a40a4e32d43edbe
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-05-14 16:10:08 +0300

    add table styles to side panel (with example)

--- page/auction-edit.php
+++ page/auction-edit.php
@@ -12,5 +12,7 @@
 	<!-- Side Panel -->
 	<div class="side-panel-list">
 		<div class="side-panel-list-item" onclick="toggleSidePanel(this, 'key-value');" title="Параметры публикации"><div class="icon fa fa-cogs"></div></div>
+		<div class="side-panel-list-item" onclick="toggleSidePanel(this, 'table');" title="Параметры публикации"><div class="icon fa fa-table"></div></div>
 	</div>
 	<?php include "../templates/side-panel/key-value.html"; ?>
+	<?php include "../templates/side-panel/table.html"; ?>
