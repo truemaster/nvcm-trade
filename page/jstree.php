12cc740a32b310dd6b84bf78bf4ae6b7417614a9
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-05-27 13:27:13 +0300

    initial jsTree commit

---
+++ page/jstree.php
@@ -0,0 +1,10 @@
+	<!-- Breadcrumbs -->
+	<?php include "../templates/breadcrumbs/jstree.html"; ?>
+	
+	<!-- Content -->
+	<div class="content">
+		
+		<!-- Section -->
+		<?php include "../templates/content/jstree.html"; ?>
+
+	</div>
