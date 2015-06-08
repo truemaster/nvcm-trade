b479c582a7d6af1b753a18b8af0938d8f9b12484
Artsiom Hvardzeitsau <artsiom.hvardzeitsau@gmail.com>
2015-06-08 13:07:26 +0300

    add demo page

---
+++ page/phase-2-fixes.php
@@ -0,0 +1,10 @@
+	<!-- Breadcrumbs -->
+	<?php include "../templates/breadcrumbs/jstree.html"; ?>
+	
+	<!-- Content -->
+	<div class="content">
+		
+		<!-- Section -->
+		<?php include "../templates/content/phase-2-fixes.html"; ?>
+
+	</div>
